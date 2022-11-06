b0e0d75dd3a7b81c7f906766c965c4fdd18bf50d
Jonathan Christian <christianj707@gmail.com>
2022-11-05 18:18:43 -0700

    Create the basics of the ComputerGame class

--- computer_game.rb
+++ computer_game.rb
@@ -1,3 +1,12 @@
 class ComputerGame
-
+  def initialize 
+    puts "Please enter a 4-digit 'master code' for the computer to break."
+    answer = gets.chomp
+    until answer.length == 4 && answer.to_i > 0
+      puts "Sorry, you must enter a 4 digit number to continue. Please try again."
+      answer = gets.chomp
+    end
+    @round = 1
+    @master_code = answer
+  end
 end
